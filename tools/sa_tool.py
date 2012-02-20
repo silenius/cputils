4ebe99eca4327bfca5f4bb446ff979d54e5ae62f
Julien Cigar <jcigar@ulb.ac.be>
2012-02-20 15:16:48 +0100

    added sa tool

---
+++ tools/sa_tool.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import cherrypy
+
+__all__ = ['SATool']
+
+
+class SATool(cherrypy.Tool):
+
+    def __init__(self):
+        super(SATool, self).__init__('on_start_resource', self.bind_session,
+                                     priority=20)
+
+    def _setup(self):
+        super(SATool, self)._setup()
+        cherrypy.serving.request.hooks.attach('on_end_resource',
+                                              self.remove_session, priority=80)
+
+    def bind_session(self):
+        self.session = cherrypy.engine.publish('get-session').pop()
+
+    def remove_session(self):
+        if not self.session:
+            return
+
+        self.session.remove()
+        self.session = None
